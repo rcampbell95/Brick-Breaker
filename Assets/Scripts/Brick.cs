d986e7820658be78d643f60ec31a047e7df6829c
Robert Campbell <robertgcampbell95@gmail.com>
2016-12-04 13:33:53 -0800

    Create remainder of brick prefabs

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Brick : MonoBehaviour {
+    public int maxHits;
+    private int timesHit;
 
 	// Use this for initialization
 	void Start () {
-	
+        timesHit = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	  if(timesHit == maxHits)
+    {
+            Destroy(this);
+
+    }
 	}
+
+    void OnCOllisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+    }
 }
